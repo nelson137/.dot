fdb2c833d665e3bc8d2d7adafc332d904fb1acfe
Nelson Earle <nelson.earle137@gmail.com>
2017-12-25 14:05:30 -0600

    made aliases not global, added pip alias

--- zsh-custom/additions.zsh
+++ zsh-custom/additions.zsh
@@ -1,17 +1,20 @@
 #!/bin/zsh
 
+alias python='python3'
+alias pip='pip3'
+
 ls_mac() {
-    alias -g ls='ls -Gp'
-    alias -g la='ls -GpA'
-    alias -g ll='ls -Gpoh'
-    alias -g lla='ls -GpAoh'
+    alias ls='ls -Gp'
+    alias la='ls -GpA'
+    alias ll='ls -Gpoh'
+    alias lla='ls -GpAoh'
 }
 
 ls_linux() {
-    alias -g ls='ls -p --color'
-    alias -g la='ls -pA --color'
-    alias -g ll='ls -poh --color'
-    alias -g lla='ls -pAoh --color'
+    alias ls='ls -p --color'
+    alias la='ls -pA --color'
+    alias ll='ls -poh --color'
+    alias lla='ls -pAoh --color'
 }
 
 
