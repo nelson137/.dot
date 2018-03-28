810fe6f014b4210cd8660d7690e04ddfe7ce8497
Nelson Earle <nelson.earle137@gmail.com>
2018-03-28 17:06:47 -0500

    updated bo/bar/brr's and jo/jar/jrr's usage

--- zsh-custom/additions.zsh
+++ zsh-custom/additions.zsh
@@ -38,7 +38,7 @@ call_instructs() { echo "Call in the same directory as FILE"; }
 bo() {
     # Build-Only
     if [[ $# != 1 ]]; then
-        echo "Usage: bo FILE" && call_instructs
+        echo "usage: bo FILE"; call_instructs
     else
         g++ "$1" -std=c++11 -o "${1%.cpp}"
     fi
@@ -47,7 +47,7 @@ bo() {
 bar() {
     # Build-And-Run
     if [[ $# == 0 ]]; then
-        echo "Usage: bar FILE" && call_instructs
+        echo "usage: bar FILE [ARGS]"; call_instructs
     else
         fn="$1"
         shift
@@ -58,7 +58,7 @@ bar() {
 brr() {
     # Build-Run-Remove
     if [[ $# == 0 ]]; then
-        echo "Usage: brr FILE" && call_instructs
+        echo "usage: brr FILE [ARGS]"; call_instructs
     else
         fn="$1"
         shift
@@ -69,7 +69,7 @@ brr() {
 jo() {
     # Java-Only
     if [[ $# != 1 ]]; then
-        echo "Usage: jo FILE" && call_instructs
+        echo "usage: jo FILE" && call_instructs
     else
         javac "$1"
     fi
@@ -78,7 +78,7 @@ jo() {
 jar() {
     # Java-And-Run
     if [[ $# != 1 ]]; then
-        echo "Usage: jar FILE" && call_instructs
+        echo "usage: jar FILE" && call_instructs
     else
         jo "$1"
         java "${1%.java}"
@@ -88,7 +88,7 @@ jar() {
 jrr() {
     # Java-Run-Remove
     if [[ $# != 1 ]]; then
-        echo "Usage: jrr FILE" && call_instructs
+        echo "usage: jrr FILE" && call_instructs
     else
         jar "$1"
         rm "${1%.java}.class"
