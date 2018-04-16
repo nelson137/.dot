c6397735fd65d4da6725be93ca50370024154cd6
Nelson Earle <nelson.earle137@gmail.com>
2018-04-16 04:04:33 -0500

    download and compile brainfuck.cpp

--- install-hooks.sh
+++ install-hooks.sh
@@ -5,11 +5,16 @@ dir="$(dirname $0)"
 cat > "${dir}/.git/hooks/post-merge" <<EOF
 #!/bin/bash
 
+dir="\$(dirname \$0)"
+
 modified="\$(git diff-tree -r --diff-filter=M --no-commit-id ORIG_HEAD HEAD)"
 modified="\$(echo "\$modified" | awk '{print \$6}')"
 
 echo "\$modified" | grep "t.cpp" >/dev/null 2>&1 && {
-    g++ t.cpp -std=c++11 -o t
+    url="https://raw.githubusercontent.com/nelson137/Brainfuck/master/interpreters/brainfuck.cpp"
+    curl -sS -o "\${dir}/brainfuck.cpp" "\$url"
+    g++ "\${dir}/brainfuck.cpp" -o "\${dir}/bin/brainfuck" -std=c++11
+    rm "\${dir}/brainfuck.cpp"
 }
 EOF
 
