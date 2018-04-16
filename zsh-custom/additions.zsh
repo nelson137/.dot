b7fbd1f6fc395fed2a67cd9440044f876b98e282
Nelson Earle <nelson.earle137@gmail.com>
2018-04-16 02:08:44 -0500

    added documentation

--- zsh-custom/additions.zsh
+++ zsh-custom/additions.zsh
@@ -4,6 +4,7 @@
 ### Config ###
  ############
 
+# Don't eat preceding space when | is typed
 export ZLE_REMOVE_SUFFIX_CHARS=$' \t\n;&'
 
 # Phantomjs
@@ -16,24 +17,28 @@ export QT_QPA_FONTDIR=/usr/share/fonts
 ### Aliases ###
  #############
 
+# ls aliases
 alias ls='ls -pv --color'
 alias la='ls -pAv --color'
 alias ll='ls -pohv --color'
 alias lla='ls -pAohv --color'
 
+# general aliases
 alias cp="cp -ir"
 alias du="du -h --max-depth=1"
 alias md="mkdir -p"
 alias mv="mv -i"
 alias rm="rm -r"
 
+# my aliases
 alias python='python3'
 alias pip='pip3'
 alias update-dot='cd ~/Projects/Git/dot && git pull && cd -'
 alias update-system='sudo apt update && sudo apt upgrade -y'
-
 alias whois='whois -H'
 
+
+# unalias gcl from .oh-my-zsh/plugins/git
 unalias gcl
 
 
@@ -43,11 +48,13 @@ unalias gcl
  ###############
 
 bat() {
+    # Get battery stats
     local stats="$(upower -i "$(upower -e | grep BAT)" | sed 's/^\s\+//g')"
     echo "$stats" | grep --color=never -E "state|to empty|percentage"
 }
 
 brightness() {
+    # Get or set screen brightness
     if [[ $# == 0 ]]; then
         eval "xrandr --verbose | grep -i brightness | awk '{print \$2}'"
         return
@@ -69,6 +76,7 @@ gcl() {
 }
 
 getip() {
+    # Get public and private ip addresses
     echo "Public:  $(curl -sS https://icanhazip.com)"
     printf "Private: "
     local cmd
