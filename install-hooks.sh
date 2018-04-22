71c49ca1915fdff66ce908254fddc8dc038cde1f
Nelson Earle <nelson.earle137@gmail.com>
2018-04-22 02:42:29 -0500

    interpreter will now be kept in Brainfuck repo

--- install-hooks.sh
+++ install-hooks.sh
@@ -7,15 +7,10 @@ cat > "${dir}/.git/hooks/post-merge" <<EOF
 
 dir="\$(dirname \$0)"
 
-modified="\$(git diff-tree -r --diff-filter=M --no-commit-id ORIG_HEAD HEAD)"
-modified="\$(echo "\$modified" | awk '{print \$6}')"
-
-echo "\$modified" | grep "t.cpp" >/dev/null 2>&1 && {
-    url="https://raw.githubusercontent.com/nelson137/Brainfuck/master/interpreters/brainfuck.cpp"
-    curl -sS -o "\${dir}/brainfuck.cpp" "\$url"
-    g++ "\${dir}/brainfuck.cpp" -o "\${dir}/bin/brainfuck" -std=c++11
-    rm "\${dir}/brainfuck.cpp"
-}
+url="https://raw.githubusercontent.com/nelson137/Brainfuck/master/interpreters/brainfuck.cpp"
+curl -sS "\$url" -o "\${dir}/brainfuck.cpp"
+g++ -std=c++11 "\${dir}/brainfuck.cpp" -o "\${dir}/bin/brainfuck"
+rm "\${dir}/brainfuck.cpp"
 EOF
 
 chmod +x "${dir}/.git/hooks/post-merge"
