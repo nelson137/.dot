db6d6387b93b8d1fd1b960a85f0a8ec3cce099db
Nelson Earle <nelson.earle137@gmail.com>
2018-04-27 15:37:25 -0500

    added hook for installing vim plugins

--- install-hooks.sh
+++ install-hooks.sh
@@ -11,6 +11,17 @@ url="https://raw.githubusercontent.com/nelson137/Brainfuck/master/interpreters/b
 curl -sS "\$url" -o "\${dir}/brainfuck.cpp"
 g++ -std=c++11 "\${dir}/brainfuck.cpp" -o "\${dir}/bin/brainfuck"
 rm "\${dir}/brainfuck.cpp"
+
+last_pull="\${dir}/last_pull"
+current="\$(git rev-parse HEAD)"
+
+git diff "\$(< \$last_pull)" "\$current" files/vimrc |
+    egrep "^(\\+|-)" | egrep -v "^(\\+|-){3}" |
+    egrep "Plugin (\\".+\\"|'.+')"
+
+[[ \$? == 0 ]] && vim +PluginClean! +PluginInstall +qall
+
+echo "\$current" > "\$last_pull"
 EOF
 
 chmod +x "${dir}/.git/hooks/post-merge"
