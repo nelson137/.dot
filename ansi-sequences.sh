3eae08d121215ec5c5a897b349d4f7120382938e
Nelson Earle <nelson.earle137@gmail.com>
2018-05-09 23:20:00 -0500

    deleted ansi-sequences.sh

--- ansi-sequences.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo abc
-echo def
-printf '\033[s'  # save pos
-printf '\033[2A'  # up one line to col 0. 3A is 2 lines
-printf '\033[2C'  # forward 2
-printf 0
-printf '\033[u'  # restore pos
