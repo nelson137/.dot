1ef0ca568178ffc49d119bd11f60ef6495d2205b
Nelson Earle <nelson.earle137@gmail.com>
2018-05-26 01:27:55 -0500

    show PULL & DIVERGED warning msgs in git prompt

---
+++ zsh-custom/plugins/git-prompt-custom/git-prompt-custom.plugin.zsh
@@ -0,0 +1,37 @@
+# vim:syntax=zsh
+
+local new_status_def=(
+    'local local_="$(git rev-parse @)"'
+    'local remote="$(git rev-parse @{u})"'
+    'local base="$(git merge-base @ @{u})"'
+    ''
+    'local custom_status'
+    'if [[ $local_ == $remote ]]; then'
+    '    true'
+    'elif [[ $local_ == $base ]]; then'
+    '    custom_status="%{$fg_bold[red]%}PULL%{$reset_color%}|"'
+    'elif [[ $remote == $base ]]; then'
+    '    true'
+    'else'
+    '    custom_status="%{$fg_bold[red]%}DIVERGED%{$reset_color%}|"'
+    'fi'
+)
+
+gss="$(which git_super_status)"
+# Line number of STATUS="$ZSH
+n="$(echo "$gss" | grep -n 'STATUS="\$ZSH' | awk '{print $1}' | cut -d ":" -f 1)"
+
+# Put each line of the git_super_status function into array lines
+lines=()
+while read -r line; do
+    lines+=( "$line" )
+done < <(echo "$gss")
+
+new_line='STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$custom_status'
+new_line+='$ZSH_THEME_GIT_PROMPT_BRANCH$GIT_BRANCH%{${reset_color}%}"'
+
+# Replace line $n with new_status_def and modified line $n
+lines=( $lines[1,$((n-1))] $new_status_def $new_line $lines[$((n+1)),$#lines] )
+# Define the modified function
+eval "${(j:
+:)lines}"  # TODO: eval "$lines" ???
