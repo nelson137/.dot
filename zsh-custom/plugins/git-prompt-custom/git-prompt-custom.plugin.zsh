f74efc4e5900851a874375e500589e9e07230c76
Nelson Earle <nelson.earle137@gmail.com>
2018-05-29 17:47:09 -0500

    nothing needs to be local, minor syntax changes

--- zsh-custom/plugins/git-prompt-custom/git-prompt-custom.plugin.zsh
+++ zsh-custom/plugins/git-prompt-custom/git-prompt-custom.plugin.zsh
@@ -1,6 +1,6 @@
 # vim:syntax=zsh
 
-local new_status_def=(
+new_status_def=(
     'local local_="$(git rev-parse @)"'
     'local url="$(git remote get-url origin)"'
     "local remote=\"\$(git ls-remote \"\$url\" | awk '/HEAD/ {print \$1}')\""
@@ -20,7 +20,7 @@ local new_status_def=(
 
 gss="$(which git_super_status)"
 # Line number of STATUS="$ZSH
-n="$(echo "$gss" | grep -n 'STATUS="\$ZSH' | awk '{print $1}' | cut -d ":" -f 1)"
+n="$(echo "$gss" | grep -n 'STATUS="\$ZSH' | awk '{print $1}' | cut -d ':' -f 1)"
 
 # Put each line of the git_super_status function into array lines
 lines=()
@@ -32,7 +32,12 @@ new_line='STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$custom_status'
 new_line+='$ZSH_THEME_GIT_PROMPT_BRANCH$GIT_BRANCH%{${reset_color}%}"'
 
 # Replace line $n with new_status_def and modified line $n
-lines=( $lines[1,$((n-1))] $new_status_def $new_line $lines[$((n+1)),$#lines] )
+lines=(
+    $lines[1,$((n-1))]
+    $new_status_def
+    $new_line
+    $lines[$((n+1)),$#lines]
+)
 # Define the modified function
 eval "${(j:
 :)lines}"
