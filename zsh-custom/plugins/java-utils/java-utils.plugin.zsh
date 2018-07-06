d4fc27eb36529fa7c8f30d06cec4b524bd4c4fb6
Nelson Earle <nelson.earle137@gmail.com>
2018-07-06 15:04:50 -0500

    improved help msgs, minor syntax changes

--- zsh-custom/plugins/java-utils/java-utils.plugin.zsh
+++ zsh-custom/plugins/java-utils/java-utils.plugin.zsh
@@ -1,28 +1,31 @@
 jo() {
     # Java Only
     if [[ $# != 1 ]]; then
-        echo 'Usage: jo FILE' >&2
+        echo 'Usage: jo <file>' >&2
         return 1
     else
         javac "$1"
     fi
 }
 
+
+
 jar() {
     # Java And Run
     if [[ $# != 1 ]]; then
-        echo 'Usage: jar FILE' >&2
+        echo 'Usage: jar <file>' >&2
         return 1
     else
-        jo "$1"
-        java "${1%.java}"
+        jo "$1" && java "${1%.java}"
     fi
 }
+
+
 
 jrr() {
     # Java, Run, Remove
     if [[ $# != 1 ]]; then
-        echo 'Usage: jrr FILE' >&2
+        echo 'Usage: jrr <file>' >&2
         return 1
     else
         jar "$1" && rm "${1%.java}.class"
