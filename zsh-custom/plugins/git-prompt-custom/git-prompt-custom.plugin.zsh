85126056e7e0b976ea091144dee64ce70f33997d
Nelson Earle <nelson.earle137@gmail.com>
2018-09-14 09:22:51 -0500

    encapsulation is not needed, minor syntax changes

--- zsh-custom/plugins/git-prompt-custom/git-prompt-custom.plugin.zsh
+++ zsh-custom/plugins/git-prompt-custom/git-prompt-custom.plugin.zsh
@@ -1,6 +1,5 @@
-# vim:syntax=zsh
-# Modifies the git_super_status function to include a waring if the
-#  current git directory is behind or has diverged from origin.
+# Modifies git_super_status function to include a waring if the
+# current git directory is behind or has diverged from origin/master.
 
 _git_pd_status() {
     local git_dir="$(git rev-parse --git-dir)"
@@ -28,57 +27,55 @@ _git_pd_status() {
     echo "$pd_status" > "$pd_status_file"
 }
 
-_git_prompt_custom() {
-    local gss="$(which git_super_status)"
 
-    # Line number of 'STATUS="$ZSH' in git_super_status()
-    local n="$(
-        echo "$gss" |
-        grep -n 'STATUS="\$ZSH' |
-        awk '{print $1}' |
-        cut -d ':' -f 1
-    )"
+# which git_super_status >/dev/null 2>&1 || return
+gss="$(which git_super_status)"
 
-    # Put each line of git_super_status() into lines
-    local -a lines
-    while read -r line; do
-        lines+=( "$line" )
-    done <<< "$gss"
+# Line number of 'STATUS="$ZSH' in git_super_status()
+n="$(
+    echo "$gss" |
+    grep -n 'STATUS="\$ZSH' |
+    awk '{print $1}' |
+    cut -d ':' -f 1
+)"
 
-    local -a pull_diverge_status
-    pull_diverge_status=(
-        'local now="$(date +%s)"'
-        'local git_dir="$(git rev-parse --git-dir)"'
-        'local last_run_file="${git_dir}/pd_status.last-run"'
-        '[[ ! -f "$last_run_file" ]] && echo "$now" > "$last_run_file"'
-        'local last_run="$(< "$last_run_file")"'
-        'local pd_status_file="${git_dir}/pd_status"'
-        'touch "$pd_status_file"'
-        'if (( now - last_run > 30 )); then'
-        '    _git_pd_status >/dev/null &'
-        '    echo "$now" > "$last_run_file"'
-        'fi'
-        'pd_status="$(< "$pd_status_file")"'
-        'local prefix="%{$fg_bold[red]%}"'
-        'local suffix="%{$reset_color%}|"'
-        'ZSH_THEME_GIT_PD_STATUS=""'
-        '[[ -n $pd_status ]] &&'
-        '    ZSH_THEME_GIT_PD_STATUS="${prefix}${pd_status}${suffix}"'
-    )
+# Put each line of git_super_status() into lines
+typeset -a lines
+while read -r line; do
+    lines+=( "$line" )
+done <<< "$gss"
 
-    local new_line_n='STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX'
-    new_line_n+='$ZSH_THEME_GIT_PD_STATUS$ZSH_THEME_GIT_PROMPT_BRANCH'
-    new_line_n+='$GIT_BRANCH%{${reset_color}%}"'
+typeset -a pull_diverge_status
+pull_diverge_status=(
+    'local now="$(date +%s)"'
+    'local git_dir="$(git rev-parse --git-dir)"'
+    'local last_run_file="${git_dir}/pd_status.last-run"'
+    '[[ ! -f "$last_run_file" ]] && echo "$now" > "$last_run_file"'
+    'local last_run="$(< "$last_run_file")"'
+    'local pd_status_file="${git_dir}/pd_status"'
+    'touch "$pd_status_file"'
+    'if (( now - last_run > 30 )); then'
+    '    _git_pd_status >/dev/null &'
+    '    echo "$now" > "$last_run_file"'
+    'fi'
+    'pd_status="$(< "$pd_status_file")"'
+    'local prefix="%{$fg_bold[red]%}"'
+    'local suffix="%{$reset_color%}|"'
+    'ZSH_THEME_GIT_PD_STATUS=""'
+    '[[ -n $pd_status ]] &&'
+    '    ZSH_THEME_GIT_PD_STATUS="${prefix}${pd_status}${suffix}"'
+)
 
-    lines=(
-        $lines[1,$((n-1))]        # Code before line n
-        $pull_diverge_status      # Pull/diverge status
-        $new_line_n               # Modified line n
-        $lines[$((n+1)),$#lines]  # Code after line n
-    )
+new_line_n='STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX'
+new_line_n+='$ZSH_THEME_GIT_PD_STATUS$ZSH_THEME_GIT_PROMPT_BRANCH'
+new_line_n+='$GIT_BRANCH%{${reset_color}%}"'
 
-    # Define the modified function
-    eval "${(j:;:)lines}"  # Join lines with ;
-}
+lines=(
+    $lines[1,$((n-1))]        # Code before line n
+    $pull_diverge_status      # Pull/diverge status
+    $new_line_n               # Modified line n
+    $lines[$((n+1)),$#lines]  # Code after line n
+)
 
-_git_prompt_custom
+# Define the modified function
+eval "${(j:;:)lines}"  # Join lines with ;
