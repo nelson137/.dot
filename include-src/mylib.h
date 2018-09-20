f376d04cc156f74949e36967314a8a1f64b7488d
Nelson Earle <nelson.earle137@gmail.com>
2018-09-20 16:27:17 -0500

    added lib-src, include-src, listbox.c, listbox.h

---
+++ include-src/mylib.h
@@ -0,0 +1,3 @@
+void lb_draw(char *choices[], int n_choices, int current, char *cursor);
+void lb_redraw(char *choices[], int n_choices, int current, char *cursor);
+int listbox(int show_instructions, char *title, char *choices[], int n_choices, char *cursor);
