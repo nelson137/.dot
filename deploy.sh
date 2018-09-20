b0e0409543a271d4c14c5591f7c86968b3fbbc0a
Nelson Earle <nelson.earle137@gmail.com>
2018-09-20 16:27:49 -0500

    added compiling, archiving mylib

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,27 @@
-#!/bin/sh
-
+#!/bin/bash
 
 here="$(dirname "$0")"
 
+no_ext() {
+    command sed -r 's/\.(c|cpp)$//' <<< "$1"
+}
+
+
 # Link config files
 command ls -A "$here/files" | xargs -I % ln -fs "$HOME/.dot/files/%" "$HOME"
+
+# Make directories for custom c libraries
+ln -fs "$HOME/.dot/include-src" "$HOME"
+mkdir -p "$HOME/.lib"
+
+# Create library
+src_fns=( $(ls "$here/lib-src") )
+for fn in "${src_fns[@]}"; do
+    gcc -std=c11 -O3 -Wall -Werror -c "$here/lib-src/$fn"
+    mv "./$(no_ext "$fn").o" "$HOME/.lib/"
+done
+ar qc "$HOME/.lib/libmylib.a" "$HOME/.lib/"*
+ranlib "$HOME/.lib/libmylib.a"
 
 # Install repository hooks
 "$here/install-hooks.sh"
