ff948bea49290a05f661513efd846ef417b934ae
Nelson Earle <nelson.earle137@gmail.com>
2018-09-22 02:18:52 -0500

    fixed removing files and spacing

--- hooks/cpp-compile.sh
+++ hooks/cpp-compile.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Exit if bin-src/ does not exist
+[[ -d bin-src ]] || exit 0
+
 
 no_ext() {
-    sed -r 's/\.(c|cpp)$//' <<< "$1"
+    sed -r 's/\.(c|cpp)$//'
 }
 
 
@@ -43,7 +46,7 @@ done < <(git diff-tree ${options/_/AM} $trees)
 # Soure code files not compiled
 typeset -a not_compiled
 while read -r fn; do
-    exe="$(no_ext "$(basename "$fn")")"
+    exe="$(basename "$fn" | no_ext)"
     [[ ! -f "bin/compiled/$exe" ]] &&
         not_compiled+=( "$(basename "$fn")" )
 done < <(find bin-src -type f)
@@ -59,11 +62,16 @@ done < <(
 )
 
 
+# Put newline between git output and compile messages
+[[ "$context" == post-merge ]] && (( "${#to_compile[@]}" > 0 )) &&
+    echo
+
+
 # Compile source code files into bin/compiled/
 for fn in "${to_compile[@]}"; do
-    echo "Compiling $fn ..."
+    echo "Compiling bin-src/$fn ..."
     ext="$(get_ext "$fn")"
-    exe="$(no_ext "$fn")"
+    exe="$(no_ext <<< "$fn")"
     if [[ "$ext" == c ]]; then
         gcc -std=c11 -O3 -Wall -Werror "bin-src/$fn" -o "bin/compiled/$exe"
     elif [[ "$ext" == cpp ]]; then
@@ -75,27 +83,41 @@ for fn in "${to_compile[@]}"; do
 done
 
 
-# Put newline between compile messages and git output
-(( "${#to_compile[@]}" > 0 )) &&
+# Put newline between compile messages and remove messages/git output
+[[ "$context" == pre-push ]] && (( "${#to_compile[@]}" > 0 )) &&
     echo
 
-# Delete binaries of removed cpp files
+
+typeset -a to_rm
 if [[ "$context" == pre-push ]]; then
     # Removed files (includes old names of renamed files)
     to_rm=(
-        $(git diff-tree ${options/_/D} "$remote_commit" "$current_commit")
+        $(git diff-tree ${options/_/D} "$remote_commit" "$current_commit" |
+            no_ext)
     )
-
-    # Remove binaries from bin/compiled/
-    for fn in "${to_rm[@]}"; do
-        exe="$(no_ext "$fn")"
-        if [[ -f "bin/compiled/$exe" ]]; then
-            echo "Removing bin/compiled/$exe ..."
-            rm -f "bin/compiled/$exe"
-        fi
-    done
+elif [[ "$context" == post-merge ]]; then
+    # Files in bin-src/ without their extensions
+    src=( $(ls -1 bin-src | no_ext) )
+    # Files in bin/compiled/
+    exe=( $(ls bin/compiled) )
+    # Files in both bin-src/ and bin/compiled/
+    intersecion=( $(printf '%s\n' "${src[@]}" "${exe[@]}" | sort | uniq -D) )
+    # Files in bin/compiled/ that aren't in bin-src/
+    # set(exe) - set(src)
+    to_rm=( $(printf '%s\n' "${intersecion[@]}" "${exe[@]}" | sort | uniq -u) )
 fi
 
+# Put newline between git output/compile messages and remove messages
+[[ "$context" == post-merge ]] && (( "${#to_rm[@]}" > 0 )) &&
+    echo
+
+# Remove binary for each removed source code file
+for fn in "${to_rm[@]}"; do
+    echo "Removing bin/compiled/$fn ..."
+    rm -f "bin/compiled/$fn"
+done
+
+
 # Put newline between removed binaries message and git output
-(( "${#to_rm[@]}" > 0 )) &&
+[[ "$context" == pre-push ]] && (( "${#to_rm[@]}" > 0 )) &&
     echo
