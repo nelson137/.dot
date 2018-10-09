a22360a135d59ca084bd3ee6b8df327ea3cd3aeb
Nelson Earle <nelson.earle137@gmail.com>
2018-10-09 13:59:36 -0500

    made c lib folder naming more logical

--- deploy.sh
+++ deploy.sh
@@ -12,13 +12,13 @@ command ls -A "$here/files" | xargs -I % ln -fs "$HOME/.dot/files/%" "$HOME"
 
 # Make directories for custom c libraries
 rm -f "$HOME/.include"
-ln -fs "$HOME/.dot/include-src" "$HOME/.include"
+ln -fs "$HOME/.dot/c-include" "$HOME/.include"
 mkdir -p "$HOME/.lib"
 
 # Create library
-src_fns=( $(ls "$here/lib-src") )
+src_fns=( $(ls "$here/c-lib-src") )
 for fn in "${src_fns[@]}"; do
-    gcc -std=c11 -O3 -Wall -Werror -c "$here/lib-src/$fn"
+    gcc -std=c11 -O3 -Wall -Werror -c "$here/c-lib-src/$fn"
     mv "./$(no_ext "$fn").o" "$HOME/.lib/"
 done
 ar qc "$HOME/.lib/libmylib.a" "$HOME/.lib/"*
