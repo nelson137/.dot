dd387cb5a9a60b12a2aa614e080a70677214ffe0
Nelson Earle <nelson.earle137@gmail.com>
2018-10-10 21:30:44 +0000

    create C libraries like HSH does

--- deploy.sh
+++ deploy.sh
@@ -10,19 +10,24 @@ no_ext() {
 # Link config files
 command ls -A "$here/files" | xargs -I % ln -fs "$HOME/.dot/files/%" "$HOME"
 
-# Make directories for custom c libraries
+# Make directories for C  libraries
 rm -f "$HOME/.include"
 ln -fs "$HOME/.dot/c-include" "$HOME/.include"
 mkdir -p "$HOME/.lib"
 
-# Create library
-src_fns=( $(ls "$here/c-lib-src") )
-for fn in "${src_fns[@]}"; do
-    gcc -std=c11 -O3 -Wall -Werror -c "$here/c-lib-src/$fn"
-    mv "./$(no_ext "$fn").o" "$HOME/.lib/"
+# Create C libraries
+lib_names=( $(ls "$here/c-lib-src") )
+for lib_name in "${lib_names[@]}"; do
+    mkdir -p "$HOME/.lib/$lib_name"
+    src_fns=( $(ls "$here/c-lib-src/$lib_name") )
+    for src_fn in "${src_fns[@]}"; do
+        obj_fn="$(no_ext "$src_fn").o"
+        gcc -std=c11 -O3 -Wall -Werror -c "$here/c-lib-src/$lib_name/$src_fn" \
+            -o "$HOME/.lib/$lib_name/$obj_fn"
+        ar rc "$HOME/.lib/lib$lib_name.a" "$HOME/.lib/$lib_name/$obj_fn"
+    done
+    ranlib "$HOME/.lib/lib$lib_name.a"
 done
-ar qc "$HOME/.lib/libmylib.a" "$HOME/.lib/"*
-ranlib "$HOME/.lib/libmylib.a"
 
 # Install repository hooks
 "$here/install-hooks.sh"
