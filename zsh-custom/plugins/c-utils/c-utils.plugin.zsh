d756f10412ac7e56f12ba4d6c77e88d9f2d42616
Nelson Earle <nelson.earle137@gmail.com>
2018-10-27 17:24:36 -0500

    g++ use same flags as gcc

--- zsh-custom/plugins/c-utils/c-utils.plugin.zsh
+++ zsh-custom/plugins/c-utils/c-utils.plugin.zsh
@@ -15,10 +15,12 @@ c() {
         return 1
     fi
 
+    local stdflags='-O3 -Wall -Werror'
     if [[ "${1%.cpp}" != "$1" ]]; then
-        g++ -std=c++11 "$1" -o "$(no_ext "$1")"
+        local cppflags="-std=c++11 $stdflags"
+        eval "g++ $cppflags $1 -o '$(no_ext "$1")' $@[2,$#]"
     elif [[ "${1%.c}" != "$1" ]]; then
-        local cflags='-std=c11 -O3 -Wall -Werror'
+        local cflags="-std=c11 $stdflags"
         eval "gcc $cflags '$1' -o '$(no_ext "$1")' $C_LD_FLAGS $@[2,$#]"
     else
         echo "File extension not recognized: $1" >&2
