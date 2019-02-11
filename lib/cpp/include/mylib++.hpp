09259b60cdfc0b50a461cc587ca5a913bca402fb
Nelson Earle <nelson.earle137@gmail.com>
2019-02-11 02:07:29 -0600

    minor syntax changes

--- lib/cpp/include/mylib++.hpp
+++ lib/cpp/include/mylib++.hpp
@@ -20,47 +20,44 @@ struct PRet {
 
 bool file_exists(string);
 
-
 string trim_whitespace(string);
 
-
 string join(vector<string>, string=" ");
-
 
 vector<string> split(string, string=" ");
 
 
 class any {
 
-    private:
-        enum type {Char, Int, Float, Double, Size_t, String};
-        char   CHAR;
-        int    INT;
-        float  FLOAT;
-        double DOUBLE;
-        size_t SIZE_T;
-        string STRING;
+private:
+    enum type {Char, Int, Float, Double, Size_t, String};
+    char   CHAR;
+    int    INT;
+    float  FLOAT;
+    double DOUBLE;
+    size_t SIZE_T;
+    string STRING;
 
-    public:
-        type m_type;
+public:
+    type m_type;
 
-        any(char   c) { this->m_type = Char;   this->CHAR   = c;         }
-        any(int    i) { this->m_type = Int;    this->INT    = i;         }
-        any(float  f) { this->m_type = Float;  this->FLOAT  = f;         }
-        any(double d) { this->m_type = Double; this->DOUBLE = d;         }
-        any(size_t s) { this->m_type = Size_t; this->SIZE_T = s;         }
-        any(string s) { this->m_type = String; this->STRING = s;         }
+    any(char   c) { this->m_type = Char;   this->CHAR   = c; }
+    any(int    i) { this->m_type = Int;    this->INT    = i; }
+    any(float  f) { this->m_type = Float;  this->FLOAT  = f; }
+    any(double d) { this->m_type = Double; this->DOUBLE = d; }
+    any(size_t s) { this->m_type = Size_t; this->SIZE_T = s; }
+    any(string s) { this->m_type = String; this->STRING = s; }
 
-        string str() {
-            switch (this->m_type) {
-                case Char:   return string(1, this->CHAR);
-                case Int:    return to_string(this->INT);
-                case Float:  return to_string(this->FLOAT);
-                case Double: return to_string(this->DOUBLE);
-                case Size_t: return to_string(this->SIZE_T);
-                case String: return this->STRING;
-            }
+    string str() {
+        switch (this->m_type) {
+            case Char:   return string(1, this->CHAR);
+            case Int:    return to_string(this->INT);
+            case Float:  return to_string(this->FLOAT);
+            case Double: return to_string(this->DOUBLE);
+            case Size_t: return to_string(this->SIZE_T);
+            case String: return this->STRING;
         }
+    }
 
 };
 
