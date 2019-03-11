c53939bcc21106928e43a3afe15ee24327ed88c2
Nelson Earle <nelson.earle137@gmail.com>
2019-03-11 13:59:03 -0500

    made (easy_)execute templates

--- lib/cpp/include/mylib++.hpp
+++ lib/cpp/include/mylib++.hpp
@@ -3,13 +3,17 @@
 #define __MYLIBPP_H
 
 
+#include <algorithm>
 #include <ios>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
 
+#include <sys/stat.h>
 #include <termios.h>
+#include <unistd.h>
+#include <wait.h>
 
 using namespace std;
 
@@ -104,9 +108,90 @@ void die(T t, Ts... ts) {
 
 bool read_fd(int, string&);
 
-int execute(exec_ret&, vector<string>&, bool=false);
+template<typename T>
+int execute(exec_ret& er, T& args, bool capture_output=false) {
+    // Get args as a vector<char*>
+    vector<char*> argv(args.size() + 1);
+    transform(args.begin(), args.end(), argv.begin(),
+        [](string& s){ return const_cast<char*>(s.c_str()); });
+    argv.push_back(NULL);
 
-exec_ret easy_execute(vector<string>&, bool=false);
+    int pipes[2][2];
+    int *out_pipe = pipes[0];
+    int *err_pipe = pipes[1];
+
+    // Create the stdout and stderr pipes
+    if (capture_output) {
+        if (pipe(out_pipe) < 0)
+            die("Could not create pipe for stdout");
+        if (pipe(err_pipe) < 0)
+            die("Could not create pipe for stderr");
+    }
+
+    int child_out_fd = out_pipe[1];
+    int child_err_fd = err_pipe[1];
+    int parent_out_fd = out_pipe[0];
+    int parent_err_fd = err_pipe[0];
+
+    int pid = fork();
+
+    if (pid == -1) {
+        die("Could not fork");
+
+    } else if (pid == 0) {
+        /**
+         * Child
+         */
+
+        if (capture_output) {
+            dup2(child_out_fd, STDOUT_FILENO);
+            dup2(child_err_fd, STDERR_FILENO);
+        }
+
+        close(child_out_fd);
+        close(child_err_fd);
+        close(parent_out_fd);
+        close(parent_err_fd);
+
+        execv(argv[0], argv.data());
+        _exit(0);
+    } else {
+        /**
+         * Parent
+         */
+
+        // Child's end of the pipes are not needed
+        close(child_out_fd);
+        close(child_err_fd);
+
+        // Wait for child to complete
+        int status;
+        waitpid(pid, &status, 0);
+
+        er.exitstatus = WEXITSTATUS(status);
+
+        if (capture_output) {
+            // Read child's stdout
+            if (! read_fd(parent_out_fd, er.out))
+                die("Could not read stdout");
+            // Read child's stderr
+            if (! read_fd(parent_err_fd, er.err))
+                die("Could not read stderr");
+        }
+
+        close(parent_out_fd);
+        close(parent_err_fd);
+    }
+
+    return er.exitstatus;
+}
+
+template<typename T>
+exec_ret easy_execute(T& args, bool capture_output=false) {
+    exec_ret er;
+    execute(er, args, capture_output);
+    return er;
+}
 
 
 class Listbox {
--- lib/cpp/src/mylib++/util.cpp
+++ lib/cpp/src/mylib++/util.cpp
@@ -1,11 +1,5 @@
-#include <algorithm>
-#include <iostream>
 #include <string>
 #include <vector>
-
-#include <sys/stat.h>
-#include <unistd.h>
-#include <wait.h>
 
 #include "mylib++.hpp"
 
@@ -84,89 +78,4 @@ bool read_fd(int fd, string& dest) {
     } while (count > 0);
 
     return true;
-}
-
-
-int execute(exec_ret& er, vector<string>& args, bool capture_output) {
-    // Get args as a vector<char*>
-    vector<char*> argv(args.size() + 1);
-    transform(args.begin(), args.end(), argv.begin(),
-        [](string& s){ return const_cast<char*>(s.c_str()); });
-    argv.push_back(NULL);
-
-    int pipes[2][2];
-    int *out_pipe = pipes[0];
-    int *err_pipe = pipes[1];
-
-    // Create the stdout and stderr pipes
-    if (capture_output) {
-        if (pipe(out_pipe) < 0)
-            die("Could not create pipe for stdout");
-        if (pipe(err_pipe) < 0)
-            die("Could not create pipe for stderr");
-    }
-
-    int child_out_fd = out_pipe[1];
-    int child_err_fd = err_pipe[1];
-    int parent_out_fd = out_pipe[0];
-    int parent_err_fd = err_pipe[0];
-
-    int pid = fork();
-
-    if (pid == -1) {
-        die("Could not fork");
-
-    } else if (pid == 0) {
-        /**
-         * Child
-         */
-
-        if (capture_output) {
-            dup2(child_out_fd, STDOUT_FILENO);
-            dup2(child_err_fd, STDERR_FILENO);
-        }
-
-        close(child_out_fd);
-        close(child_err_fd);
-        close(parent_out_fd);
-        close(parent_err_fd);
-
-        execv(argv[0], argv.data());
-        _exit(0);
-    } else {
-        /**
-         * Parent
-         */
-
-        // Child's end of the pipes are not needed
-        close(child_out_fd);
-        close(child_err_fd);
-
-        // Wait for child to complete
-        int status;
-        waitpid(pid, &status, 0);
-
-        er.exitstatus = WEXITSTATUS(status);
-
-        if (capture_output) {
-            // Read child's stdout
-            if (! read_fd(parent_out_fd, er.out))
-                die("Could not read stdout");
-            // Read child's stderr
-            if (! read_fd(parent_err_fd, er.err))
-                die("Could not read stderr");
-        }
-
-        close(parent_out_fd);
-        close(parent_err_fd);
-    }
-
-    return er.exitstatus;
-}
-
-
-exec_ret easy_execute(vector<string>& args, bool capture_output) {
-    exec_ret er;
-    execute(er, args, capture_output);
-    return er;
 }
