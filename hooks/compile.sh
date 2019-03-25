35671975fb4dff7cb5f8b491da690b90b995aa08
Nelson Earle <nelson.earle137@gmail.com>
2019-03-25 02:17:40 -0500

    updated syntax for to program

--- hooks/compile.sh
+++ hooks/compile.sh
@@ -73,9 +73,9 @@ for fn in "${to_compile[@]}"; do
     ext="$(get_ext "$fn")"
     exe="$(no_ext <<< "$fn")"
     if [[ "$ext" == c ]]; then
-        to cf -o "bin/compiled/$exe" "bin-src/$fn"
+        to cof "bin/compiled/$exe" "bin-src/$fn"
     elif [[ "$ext" == cpp ]]; then
-        to cf -o "bin/compiled/$exe" "bin-src/$fn"
+        to cof "bin/compiled/$exe" "bin-src/$fn"
     else
         printf "\r\033[A"
         echo "compile.sh: filetype not recognized: $fn" >&2
