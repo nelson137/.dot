c8415f2c152de51dbde1d6ada5d4cb80780982c7
Nelson Earle <nelson.earle137@gmail.com>
2019-12-20 15:54:37 -0600

    remove my c and c++ libs

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,6 @@ listdir() {
     find "$1" -maxdepth 1 | sed 1d
 }
 
-no_ext() {
-    sed -E 's/\.(c|cpp)$//' <<< "$1"
-}
-
-lang() {
-    grep -q '\.c$' <<< "$1" && echo c || echo cpp
-}
-
-
 # Clone all submodules
 git submodule update --init --recursive
 
@@ -29,32 +20,6 @@ listdir "$here/.config" | xargs -I % ln -fs % "$HOME/.config"
 # Link vim autoload files
 mkdir -p "$HOME/.vim/autoload"
 ln -fs "$here/.vim/autoload/"* "$HOME/.vim/autoload"
-
-# Make directories for libraries
-rm -rf "$HOME"/.{include,lib}
-mkdir "$HOME"/.{include,lib}
-
-# Create C and C++ libraries
-for lib in $(ls "$here/lib"); do
-    ln -fs "$here/lib/$lang/$lib/include/"* "$HOME/.include/$lang/"
-    mkdir -p "$HOME/.lib/$lang/$lib"
-    for src_fn in $(ls "$here/lib/$lang/$lib/src"); do
-        obj_fn="$(no_ext "$src_fn").o"
-        if [[ "$(lang "$src_fn")" == 'c' ]]; then
-            gcc -std=c11 -O3 -Wall -Werror -c \
-                "$here/lib/$lib/src/$src_fn" \
-                -o "$HOME/.lib/$lib/$obj_fn" \
-                -I"$HOME/.include"
-        else
-            g++ -std=c++17 -O3 -Wall -Werror -c \
-                "$here/lib/$lib/src/$src_fn" \
-                -o "$HOME/.lib/$lib/$obj_fn" \
-                -I"$HOME/.include"
-        fi
-        ar rc "$HOME/.lib/lib$lib.a" "$HOME/.lib/$lib/$obj_fn"
-    done
-    ranlib "$HOME/.lib/lib$lib.a"
-done
 
 # Install repository hooks
 "$here/install-hooks.sh"
--- lib/mylib-c/include/mylib.h
+++
@@ -1,3 +0,0 @@
-void lb_draw(char *choices[], int n_choices, int current, char *cursor);
-void lb_redraw(char *choices[], int n_choices, int current, char *cursor);
-int listbox(int show_instructions, char *title, char *choices[], int n_choices, char *cursor);
