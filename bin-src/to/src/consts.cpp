fcd4acd32e3032a90d2feb614faff459e8bb4924
Nelson Earle <nelson.earle137@gmail.com>
2020-06-27 14:52:08 -0500

    to: replace c++ proj with rust repo as submod

--- bin-src/to/src/consts.cpp
+++
@@ -1,4 +0,0 @@
-#include "consts.hpp"
-
-
-string USAGE = "Usage: to [-h] <commands> <infile> [outfile] [lang] [ARGS...]";
--- bin-src/to/src/main.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_HPP
-#define MAIN_HPP
-
-
-#include "to.hpp"
-
-
-#endif
