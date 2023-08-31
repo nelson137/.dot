d078a455b7c6a138b788b45d2a3f4389f3304d81
Nelson Earle <nelson.earle137@gmail.com>
2023-08-30 23:43:19 -0500

    nvim: add bufferline

---
+++ config/nvim/lua/plugins/modules/bufferline.lua
@@ -0,0 +1,40 @@
+-- Fancy buffer line (instead of the default tab line)
+
+vim.opt.termguicolors = true
+vim.opt.mousemoveevent = true
+
+return {
+    'akinsho/bufferline.nvim',
+
+    dependencies = {
+        'nvim-tree/nvim-web-devicons',
+    },
+
+    opts = {
+        options = {
+            diagnostics = 'nvim_lsp',
+            diagnostics_indicator = function(count, level, diagnostics_dict)
+                local errors = diagnostics_dict['error']
+                local warnings = diagnostics_dict['warning']
+                local infos = diagnostics_dict['info']
+                local sections = {}
+                if errors and errors > 0 then table.insert(sections, errors .. '') end
+                if warnings and warnings > 0 then table.insert(sections, warnings .. '') end
+                if infos and infos > 0 then table.insert(sections, infos .. '') end
+                return table.concat(sections, ' ')
+            end,
+            hover = {
+                enabled = true,
+                delay = 0,
+                reveal = { 'close' },
+            },
+            left_trunc_marker = '⟵',
+            right_trunc_marker = '⟶',
+            sort_by = 'insert_at_end',
+        },
+    },
+}
+
+
+
+
