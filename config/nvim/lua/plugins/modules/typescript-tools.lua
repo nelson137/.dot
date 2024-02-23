fb2c44eeda05b4d05e87da5c09c0d47ac292554a
Nelson Earle <nelson.earle137@gmail.com>
2024-02-23 15:31:58 -0600

    nvim: add TS server config

---
+++ config/nvim/lua/plugins/modules/typescript-tools.lua
@@ -0,0 +1,9 @@
+-- Typescript LSP that improves on `typescript-language-server`
+
+return {
+    'pmizio/typescript-tools.nvim',
+
+    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+
+    config = true,
+}
