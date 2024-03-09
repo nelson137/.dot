7e6b7fa5c3aeeae5bc14a08515fd58cf8c415037
Nelson Earle <nelson.earle137@gmail.com>
2024-03-08 19:08:39 -0600

    nvim: add angularls

--- config/nvim/lua/plugins/modules/mason-lspconfig.lua
+++ config/nvim/lua/plugins/modules/mason-lspconfig.lua
@@ -9,6 +9,7 @@ return {
 
     opts = {
         ensure_installed = {
+            'angularls@15.2.0',
             'eslint',
             'lua_ls',
             'rust_analyzer',
@@ -17,6 +18,35 @@ return {
         handlers = {
             function(server)
                 require('lspconfig')[server].setup({})
+            end,
+            ['angularls'] = function()
+                local util = require('lspconfig.util')
+
+                local get_probe_dir = function(root_dir)
+                    local project_root = util.find_node_modules_ancestor(root_dir)
+                    return project_root and (project_root .. '/node_modules') or ''
+                end
+
+                local default_probe_dir = get_probe_dir(vim.fn.getcwd())
+
+                require('lspconfig')['angularls'].setup({
+                    cmd = {
+                        'ngserver',
+                        '--stdio',
+                        '--tsProbeLocations', default_probe_dir,
+                        '--ngProbeLocations', default_probe_dir,
+                    },
+                    root_dir = util.root_pattern('angular.json', 'nx.json'),
+                    on_new_config = function(new_config, new_root_dir)
+                        local new_probe_dir = get_probe_dir(new_root_dir)
+                        new_config.cmd = {
+                            'ngserver',
+                            '--stdio',
+                            '--tsProbeLocations', new_probe_dir,
+                            '--ngProbeLocations', new_probe_dir,
+                        }
+                    end,
+                })
             end,
             ['rust_analyzer'] = function()
                 -- noop
