a7370927cf346bf7394abdb54af5ca0a79fa7884
Nelson Earle <nelson.earle137@gmail.com>
2024-03-30 12:53:45 -0500

    nvim: simplify illuminate plugin config

--- config/nvim/lua/plugins/modules/illuminate.lua
+++ config/nvim/lua/plugins/modules/illuminate.lua
@@ -5,19 +5,11 @@ return {
 
     event = 'VeryLazy',
 
-    config = function(_, opts)
-        require('illuminate').configure({
-            delay = 300,
-        })
+    opts = {
+        delay = 300,
+    },
 
-        vim.cmd([[
-            " cterm=235 :: gui=#262626
-            " cterm=236 :: gui=#303030
-            " cterm=237 :: gui=#3a3a3a
-            hi! _IlluminatedShared cterm=NONE ctermbg=237 gui=NONE guibg=#3a3a3a
-            hi! default link IlluminatedWordRead _IlluminatedShared
-            hi! default link IlluminatedWordText _IlluminatedShared
-            hi! default link IlluminatedWordWrite _IlluminatedShared
-        ]])
+    config = function(_, opts)
+        require('illuminate').configure(opts)
     end,
 }
