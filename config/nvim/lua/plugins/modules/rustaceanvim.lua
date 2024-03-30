16ef39788e0c2c195e81b33dd35a3150c9414f77
Nelson Earle <nelson.earle137@gmail.com>
2024-03-30 13:01:19 -0500

    nvim: add rust-analyzer settings

--- config/nvim/lua/plugins/modules/rustaceanvim.lua
+++ config/nvim/lua/plugins/modules/rustaceanvim.lua
@@ -1,5 +1,37 @@
 -- Configure Rust language server
 
+local function code_action() vim.cmd.RustLsp('codeAction') end
+
+local function on_attach(_, bufnr)
+    local map = function(mode, lhs, rhs, desc)
+        local opts = { buffer = bufnr, desc = 'RustLsp: ' .. desc }
+        vim.keymap.set(mode, lhs, rhs, opts)
+    end
+    map('n', '<Leader>.', code_action, 'codeAction')
+end
+
+local rust_analyzer_settings = {
+    completion = {
+        callable = {
+            snippets = 'add_parentheses',
+        },
+    },
+    imports = {
+        granularity = {
+            enforce = true,
+        },
+    },
+}
+
+vim.g.rustaceanvim = {
+    server = {
+        on_attach = on_attach,
+        default_settings = {
+            ['rust-analyzer'] = rust_analyzer_settings,
+        },
+    },
+}
+
 return {
     'mrcjkb/rustaceanvim',
 
