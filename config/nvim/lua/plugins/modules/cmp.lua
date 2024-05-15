a2f11acd469e9a1895409d0118407d625beed37f
Nelson Earle <nelson.earle137@gmail.com>
2024-05-15 11:55:35 -0500

    feat(nvim): replace on completion confirm

--- config/nvim/lua/plugins/modules/cmp.lua
+++ config/nvim/lua/plugins/modules/cmp.lua
@@ -64,7 +64,7 @@ return {
                 ['<C-e>'] = cmp.mapping.close(),
 
                 ['<CR>'] = cmp.mapping.confirm({
-                    behavior = cmp.ConfirmBehavior.Insert,
+                    behavior = cmp.ConfirmBehavior.Replace,
                     select = false,
                 }),
             },
