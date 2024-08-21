e293f067106713cf89261c52c6cd8e388d1e6d4c
Nelson Earle <nelson.earle137@gmail.com>
2024-08-21 16:36:35 -0500

    feat(nvim): simplify completion mappings
    Use `<C-n>` & `<C-p>` instead of `<Tab>` & `<S-Tab>`.

--- config/nvim/lua/plugins/modules/cmp.lua
+++ config/nvim/lua/plugins/modules/cmp.lua
@@ -1,35 +1,23 @@
 -- Autocompletion
 
-local has_words_before = function()
-    unpack = unpack or table.unpack
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local feedkey = function(key, mode)
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+local feedkey = function(keys, mode)
+    local resolved_keys = vim.api.nvim_replace_termcodes(keys, true, true, true)
+    vim.api.nvim_feedkeys(resolved_keys, mode or '', true)
 end
 
 local action_tab = function(fallback)
-    local cmp = require('cmp')
-    if cmp.visible() then
-        cmp.select_next_item()
-    elseif vim.fn['vsnip#available'](1) == 1 then
-        feedkey('<Plug>(vsnip-expand-or-jump)', '')
-    elseif has_words_before() then
-        cmp.complete()
+    if vim.fn['vsnip#jumpable'](1) == 1 then
+        feedkey('<Plug>(vsnip-jump-next)')
     else
-        -- The fallback function sends an already mapped key. In this case, it's probably `<Tab>`.
-        fallback()
+        fallback() -- sends the mapped key
     end
 end
 
-local action_shift_tab = function()
-    local cmp = require('cmp')
-    if cmp.visible() then
-        cmp.select_prev_item()
-    elseif vim.fn['vsnip#jumpable'](-1) == 1 then
-        feedkey('<Plug>(vsnip-jump-prev)', '')
+local action_shift_tab = function(fallback)
+    if vim.fn['vsnip#jumpable'](-1) == 1 then
+        feedkey('<Plug>(vsnip-jump-prev)')
+    else
+        fallback() -- sends the mapped key
     end
 end
 
@@ -40,6 +28,7 @@ return {
     -- See [config options](https://github.com/hrsh7th/nvim-cmp#basic-configuration)
     opts = function()
         local cmp = require('cmp')
+        -- cmp.mapping
         return {
             preselect = cmp.PreselectMode.None,
             snippet = {
@@ -48,11 +37,8 @@ return {
                 end,
             },
             mapping = {
-                -- ['<C-p>'] = cmp.mapping.select_prev_item(),
-                -- ['<C-n>'] = cmp.mapping.select_next_item(),
-
-                -- ['<Tab>'] = cmp.mapping.select_next_item(),
-                -- ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+                ['<C-p>'] = cmp.mapping.select_prev_item(),
+                ['<C-n>'] = cmp.mapping.select_next_item(),
 
                 ['<Tab>'] = cmp.mapping(action_tab, { 'i', 's' }),
                 ['<S-Tab>'] = cmp.mapping(action_shift_tab, { 'i', 's' }),
@@ -60,9 +46,7 @@ return {
                 ['<C-k>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-j>'] = cmp.mapping.scroll_docs(4),
 
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<C-e>'] = cmp.mapping.close(),
-
+                ['<C-]>'] = cmp.mapping.close(),
                 ['<CR>'] = cmp.mapping.confirm({
                     behavior = cmp.ConfirmBehavior.Replace,
                     select = false,
