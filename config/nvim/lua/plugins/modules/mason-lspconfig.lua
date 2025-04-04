cf6b8beff074ad6db381933cf726c38a4773ae44
Nelson Earle <nelson.earle137@gmail.com>
2025-04-04 14:11:12 -0500

    feat(nvim): ensure tsserver is installed

--- config/nvim/lua/plugins/modules/mason-lspconfig.lua
+++ config/nvim/lua/plugins/modules/mason-lspconfig.lua
@@ -13,6 +13,7 @@ return {
             'eslint',
             'lua_ls',
             'rust_analyzer',
+            'ts_ls',
         },
 
         handlers = {
@@ -64,6 +65,10 @@ return {
 
             rust_analyzer = function()
                 -- noop
+            end,
+
+            ts_ls = function()
+                -- noop, handled by `typescript-tools.nvim`
             end,
         },
     },
