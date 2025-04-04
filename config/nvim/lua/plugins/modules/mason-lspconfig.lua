ecbeb52560f7ce38241bb7f1858b6bb22f9896e2
Nelson Earle <nelson.earle137@gmail.com>
2025-04-04 16:04:02 -0500

    feat(nvim): simplify angularls config

--- config/nvim/lua/plugins/modules/mason-lspconfig.lua
+++ config/nvim/lua/plugins/modules/mason-lspconfig.lua
@@ -24,30 +24,9 @@ return {
             angularls = function()
                 local util = require('lspconfig.util')
 
-                local get_probe_dir = function(root_dir)
-                    return vim.fs.find('node_modules', { path = root_dir, upward = true })[1] or ''
-                end
-
-                local default_probe_dir = get_probe_dir(vim.fn.getcwd())
-
                 require('lspconfig').angularls.setup({
-                    cmd = {
-                        'ngserver',
-                        '--stdio',
-                        '--tsProbeLocations', default_probe_dir,
-                        '--ngProbeLocations', default_probe_dir,
-                    },
                     filetypes = { 'html' },
                     root_dir = util.root_pattern('angular.json', 'nx.json'),
-                    on_new_config = function(new_config, new_root_dir)
-                        local new_probe_dir = get_probe_dir(new_root_dir)
-                        new_config.cmd = {
-                            'ngserver',
-                            '--stdio',
-                            '--tsProbeLocations', new_probe_dir,
-                            '--ngProbeLocations', new_probe_dir,
-                        }
-                    end,
                 })
             end,
 
