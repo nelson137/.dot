7436a97b2de065739bbc0c3acec631ff64216a3f
Nelson Earle <nelson.earle137@gmail.com>
2025-09-25 10:12:30 -0500

    feat(nvim): enable ng plugin & fix keymaps

--- config/nvim/lua/plugins/modules/ng.lua
+++ config/nvim/lua/plugins/modules/ng.lua
@@ -2,19 +2,18 @@
 
 return {
     'joeveiga/ng.nvim',
-    enabled = false,
 
     config = function()
         local ng = require('ng')
 
         local map = Map('Angular')
 
-        local function goto_template()
-            ng.goto_template_for_component({ reuse_window = true })
-        end
-
         local function goto_component()
             ng.goto_component_with_template_file({ reuse_window = true })
+        end
+
+        local function goto_template()
+            ng.goto_template_for_component({ reuse_window = true })
         end
 
         map('n', '<Leader>at', goto_template, 'go to component template')
